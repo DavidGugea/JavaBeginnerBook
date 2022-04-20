6328a2ff6962dcc57a660264dfdc0cbb2254c47a
David Gugea <davidgugea@yahoo.com>
2022-04-20 21:06:20 +0200

    Basic Character and String methods/static functions

---
+++ Chapter5/CharacterTest.java
@@ -0,0 +1,6 @@
+public class CharacterTest {
+    public static void main(String[] args) {
+        System.out.println(String.valueOf(12));
+        System.out.println(Character.toString('h'));
+    }
+}
